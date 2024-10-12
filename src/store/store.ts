0e4ea4040df39f0f0330d508de8718e604665467
Naimur00 <aminaim4u@gmail.com>
2024-10-12 12:31:56 +0600

    add: RootState & AppDispatch in stores

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from './todo-slice'
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         todos: todosReducer
     },
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
